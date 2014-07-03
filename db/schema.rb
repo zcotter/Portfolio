4c4a21f67a0ea6896a6e13c1fb24522cab0d04bf
Zach Cotter <cotter.z@husky.neu.edu>
2014-07-03 10:48:45 -0400

    added languages column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130902143717) do
+ActiveRecord::Schema.define(:version => 20140703144649) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20130902143717) do
     t.string   "lead"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "language"
   end
 
   create_table "users", :force => true do |t|
