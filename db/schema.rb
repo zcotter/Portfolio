53a750d3eb9ddeaf4a7854358c28706749a66bc6
Zach Cotter <cotter.z@husky.neu.edu>
2014-07-08 09:13:27 -0400

    photos should have comments which are displayed in the post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708112628) do
+ActiveRecord::Schema.define(version: 20140708114100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140708112628) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "caption"
   end
 
   create_table "posts", force: true do |t|
