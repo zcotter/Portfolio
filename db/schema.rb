e71c1f46739898699f34ef99fff05604224285b2
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-20 15:33:03 -0400

    add registration id to bananagrams players

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019182417) do
+ActiveRecord::Schema.define(version: 20141020193223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141019182417) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "registration_id"
   end
 
   create_table "bananagrams_score", force: true do |t|
