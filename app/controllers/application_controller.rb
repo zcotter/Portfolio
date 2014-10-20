7d2ceb432d2a21d715b50715fa90fc1a18d332f3
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-19 22:55:33 -0400

    fixed new viewer bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,25 @@ class ApplicationController < ActionController::Base
   before_action :stalk_viewer
 
   def all_projects
-    @projects = Project.all
+    begin
+      @projects = Project.all
+    rescue
+    end
   end
 
   def stalk_viewer
-    ip = request.remote_addr
+    begin
+      ip = request.remote_addr
 
-    if Viewer.where(ip: ip).size != 0
-      viewer = Viewer.where(ip: ip).first
-    else
-      viewer = Viewer.new({ip: ip})
-      viewer.save!
+      if Viewer.where(ip: ip).size != 0
+        viewer = Viewer.where(ip: ip).first
+      else
+        viewer = Viewer.new({ip: ip})
+        viewer.save!
+      end
+      view = View.new({viewer_id: viewer.id, path: request.path_info, at: Time.now, user_agent: request.user_agent})
+      view.save!
+    rescue
     end
-    view = View.new({viewer_id: viewer.id, path: request.path_info, at: Time.now, user_agent: request.user_agent})
-    view.save!
   end
 end
