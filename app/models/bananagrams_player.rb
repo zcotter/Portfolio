699bbf18ec6172cae9e9f5e29b482b71d7a44375
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-19 22:33:52 -0400

    add high score table to index

--- app/models/bananagrams_player.rb
+++ app/models/bananagrams_player.rb
@@ -1,3 +1,7 @@
 class BananagramsPlayer < ActiveRecord::Base
   has_many :bananagrams_scores, class_name: 'BananagramsScore'
+
+  def top_score
+    BananagramsScore.where(bananagrams_player: self).order(:score).last
+  end
 end
