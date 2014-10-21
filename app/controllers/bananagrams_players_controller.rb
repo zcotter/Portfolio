8a23e4a48d7dccaa3f553ee4909f916b34ac4e23
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-21 14:14:22 -0400

    add index to bananagrams players controller

--- app/controllers/bananagrams_players_controller.rb
+++ app/controllers/bananagrams_players_controller.rb
@@ -1,4 +1,10 @@
 class BananagramsPlayersController < ApplicationController
+
+  def index
+    players = BananagramsPlayer.all
+    render json: players.to_json
+  end
+
   def create
     @player = BananagramsPlayer.new(player_params)
     if @player.save!
