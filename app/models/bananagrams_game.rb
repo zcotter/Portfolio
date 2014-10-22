a8b5b2fb995d89f697c67177b2d62f40e44b34ff
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-22 00:13:45 -0400

    Added bananagrams game and gcm dependency

---
+++ app/models/bananagrams_game.rb
@@ -0,0 +1,13 @@
+class BananagramsGame < ActiveRecord::Base
+  belongs_to :first_player, class_name: 'BananagramsPlayer', foreign_key: 'first_player_id'
+  belongs_to :second_player, class_name: 'BananagramsPlayer', foreign_key: 'second_player_id'
+
+  after_save :notify_second_player
+
+  def notify_second_player
+    gcm = GCM.new(ENV["GCM_API_KEY"])
+    reg_ids = [self.second_player.registration_id]
+    options = {data: {type: 'GameInvitation', game_id: self.id}}
+    response = gcm.send(reg_ids, options)
+  end
+end
--- app/models/bananagrams_player.rb
+++ app/models/bananagrams_player.rb
@@ -1,5 +1,6 @@
 class BananagramsPlayer < ActiveRecord::Base
   has_many :bananagrams_scores, class_name: 'BananagramsScore'
+  has_many :bananagrams_games, class_name: 'BananagramsGame'
 
   def top_score
     BananagramsScore.where(bananagrams_player: self).order(:score).last
