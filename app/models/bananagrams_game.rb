82986dd2e0f5929b9a4f653cd93ca13291da917e
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-22 13:45:14 -0400

    game invitation notifier should also send requester name

--- app/models/bananagrams_game.rb
+++ app/models/bananagrams_game.rb
@@ -7,7 +7,7 @@ class BananagramsGame < ActiveRecord::Base
   def notify_second_player
     gcm = GCM.new(ENV["GCM_API_KEY"])
     reg_ids = [self.second_player.registration_id]
-    options = {data: {type: 'GameInvitation', game_id: self.id}}
+    options = {data: {type: 'GameInvitation', game_id: self.id, requester: self.second_player.name}}
     response = gcm.send(reg_ids, options)
   end
 end
