a995160a76869629731468f7b5dae9e592c81659
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-22 00:13:45 -0400

    Added bananagrams game and gcm dependency

---
+++ app/controllers/bananagrams_game_controller.rb
@@ -0,0 +1,20 @@
+class BananagramsPlayersController < ApplicationController
+
+
+  def create
+    game = game_params
+    game[:first_player] = BananagramsPlayer.find(game[:first_player])
+    game[:second_player] = BananagramsPlayer.find(game[:second_player])
+    @game = BananagramsGame.new(game_params)
+    if @game.save!
+      render json: @game.to_json
+      # TODO notify :second_player
+    else
+      head :bad_request, content_type: 'application/json'
+    end
+  end
+
+  def game_params
+    params.require(:bananagrams_game).permit(:first_player, :second_player)
+  end
+end
