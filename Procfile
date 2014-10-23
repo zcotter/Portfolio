a4cfc0b3ad757b17522ee3754545c23a7ac9dc27
Zach Cotter <cotter.z@husky.neu.edu>
2014-10-23 17:40:37 -0400

    add delayed job

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec unicorn -D -p 80 -c ./config/unicorn.rb
+worker: bundle exec rake jobs:work
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022022558) do
+ActiveRecord::Schema.define(version: 20141023212848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,22 @@ ActiveRecord::Schema.define(version: 20141022022558) do
 
   add_index "bananagrams_scores", ["bananagrams_player_id"], name: "index_bananagrams_scores_on_bananagrams_player_id", using: :btree
   add_index "bananagrams_scores", ["score"], name: "index_bananagrams_scores_on_score", using: :btree
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "post_photos", force: true do |t|
     t.integer  "post_id"
