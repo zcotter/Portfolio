95bd2e16842a3db2278f4d943cad4eabbf531e7d
Zach Cotter <cotter.z@husky.neu.edu>
2014-12-16 01:35:23 -0500

    reduce unicorn workers

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # config/unicorn.rb
-worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 1)
 timeout 30
 preload_app true
 working_directory "/home/zcotter/Portfolio"
