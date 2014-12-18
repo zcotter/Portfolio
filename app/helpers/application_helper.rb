c5d56fcaa88f9bbacadf97f5bd02031f2c9f7298
Zach Cotter <cotter.z@husky.neu.edu>
2014-12-17 23:54:27 -0600

    active page should be highlighted in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def nav_active(controller_name = nil, action_name = nil)
+    if (!controller_name || controller_name == controller.controller_name) &&
+       (!action_name || action_name == controller.action_name)
+       'active'
+     else
+       ''
+     end
+  end
 end
